c5f4680964df30fbbf852a94886f0c46fb94d01f
BuildTools <unconfigured@null.spigotmc.org>
2023-01-28 22:19:56 +0100

    init

--- src/main/java/de/fabidev/utils/PlotChangeManager.java
+++ src/main/java/de/fabidev/utils/PlotChangeManager.java
@@ -1,6 +1,8 @@
 package de.fabidev.utils;
 
+import com.plotsquared.core.configuration.ConfigurationUtil;
 import com.plotsquared.core.player.PlotPlayer;
+import com.plotsquared.core.plot.BlockBucket;
 import com.plotsquared.core.plot.Plot;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -11,8 +13,18 @@ public class PlotChangeManager {
 
         PlotPlayer<Player> plotPlayer = PlotPlayer.from(player);
         Plot plot = plotPlayer.getCurrentPlot();
+        Pattern
         if (plot != null){
+            if(plot.getConnectedPlots().size() >1){
+                for (Plot plots : plot.getConnectedPlots()){
+                    if (plots.getOwners().contains(player.getUniqueId()) || player.hasPermission(Strings.permissionAdmin)){
+                        plots.getPlotModificationManager().setComponent(Material.QUARTZ_BRICKS, , null, null);
 
+                    }
+                }
+            }
+        }else{
+            player.sendMessage(Strings.notOnPlot);
         }
 
     }
---
+++ src/main/java/de/fabidev/utils/Strings.java
@@ -0,0 +1,11 @@
+package de.fabidev.utils;
+
+public class Strings {
+    private static String prefix = "§4[§6§lCityBuild§4]§r ";
+    public static String notOnPlot = prefix + "§4Du befindest dich auf keinem Plot!";
+
+
+    public static String permissionAdmin = "wandrand.admin";
+
+
+}
