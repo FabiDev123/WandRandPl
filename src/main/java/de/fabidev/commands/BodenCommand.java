95b950df8de506976192a265a20589b497dcafa5
BuildTools <unconfigured@null.spigotmc.org>
2023-02-19 12:58:17 +0100

    Finalized plugin incl GUI etc.

--- src/main/java/de/fabidev/commands/BodenCommand.java
+++ src/main/java/de/fabidev/commands/BodenCommand.java
@@ -1,19 +1,7 @@
 package de.fabidev.commands;
 
-import com.plotsquared.core.PlotAPI;
-import com.plotsquared.core.PlotSquared;
-import com.plotsquared.core.configuration.ConfigurationUtil;
-import com.plotsquared.core.location.Location;
-import com.plotsquared.core.player.PlotPlayer;
-import com.plotsquared.core.plot.BlockBucket;
-import com.plotsquared.core.plot.Plot;
-import com.plotsquared.core.util.RegionManager;
-import com.sk89q.worldedit.function.pattern.Pattern;
-import de.fabidev.Wandrand;
 import de.fabidev.utils.GUI;
-import de.fabidev.utils.PlotChangeManager;
 import de.fabidev.utils.Strings;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -26,7 +14,7 @@ public class BodenCommand implements CommandExecutor {
 
         if(sender instanceof Player){
             Player p = (Player) sender;
-            if (p.hasPermission(Strings.permissionRand) || p.hasPermission(Strings.permissionAdmin)){
+            if (p.hasPermission(Strings.permissionBoden) || p.hasPermission(Strings.permissionAdmin)){
                 GUI.openGUIBoden(p, 1);
             }else{
                 p.sendMessage(Strings.noPermission);
